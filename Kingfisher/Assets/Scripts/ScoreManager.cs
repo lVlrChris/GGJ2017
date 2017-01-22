c5fd958f4d9e9874a74965c3257d860551fcf2f9
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2017-01-22 09:33:39 +0100

    timer

--- Kingfisher/Assets/Scripts/ScoreManager.cs
+++ Kingfisher/Assets/Scripts/ScoreManager.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
 
     public int score;
-    
+    private float startTime;
+    private float playTime;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    startTime = Time.time;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+	{
+	    playTime = Time.time - startTime;
+        GameObject.FindGameObjectWithTag("Timer").GetComponent<Text>().text = "" + playTime;
+
+    }
 }
