bc4258b596dd6e1a5380854ff4100cc310e54d59
Owen Hoogenboezem <Owen.hoogenboezem@student.hu.nl>
2017-01-22 10:05:45 +0100

    turtle animation

---
+++ Kingfisher/Assets/Scripts/Turtle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour
+{
+    private bool animationPlaying;
+    public int animationPlayTime = 4;
+    private int animationCount = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void PlayAnimation()
+    {
+        animationPlaying = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if (animationPlaying)
+	    {
+	        if (animationCount < animationPlayTime)
+	        {
+	            GetComponent<Animation>().Play();
+                animationCount++;
+	        }
+	    }
+		
+	}
+}
